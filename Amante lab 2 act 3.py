cd5207b37ca0d40203b93c0c8331b1091c1c8fcd
Swagadon540 <donrejamante.dr@gmail.com>
2025-03-08 10:25:34 +0800

    Add files via upload

---
+++ Amante lab 2 act 3.py
@@ -0,0 +1,16 @@
+#agwat
+def classify_age(age):
+    if age < 0:
+        print("Invalid age.")
+    elif age <= 12:
+        print("You are a Child.")
+    elif age <= 19:
+        print("You are a Teen.")
+    elif age <= 64:
+        print("You are an Adult.")
+    else:
+        print("You are a Senior.")
+
+# main q
+age = int(input("Enter your age: "))
+classify_age(age)
